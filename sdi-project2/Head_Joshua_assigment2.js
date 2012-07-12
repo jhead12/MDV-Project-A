fa035776b1647feaa2cd4483f2af0d662e65951e
Josh <jhead12@fullsail.edu>
2012-07-12 01:35:18 -0700

    Week2 Assignment

--- sdi-project2/Head_Joshua_assigment2.js
+++ sdi-project2/Head_Joshua_assigment2.js
@@ -1,1 +1,62 @@
-// alert("JavaScript works!");
+/*
+	Joshua Head
+	Assignment 2
+	SDI 1207
+*/
+
+
+
+/*
+	
+	# The contestant name var contestant ="John"; had been cooking his first meal in the contest for
+cooking time var cookTime = 20; and he still had to put in the second food item. He only cooked some
+foodItems = [ "chicken" , "vegetables", "potatoes", "desert"].
+
+ Is time to var cookDish = function(meal){...};? It looks like I have var mealsLeft = meals.length; to
+go. The Judges say that there are 30 Mins left, and if the competition (mealsLeft > 0) ends.
+
+ As the "John" looks at the clock the time is wining down var timeLeft = 0; timeNumber < time.length;
+timeNumber--), There we there was a missing item that was needed missingItem = function(call,
+words){...}. the result came out to be that: return "there are none of the items that you are looking
+for!"
+*/
+
+
+var contestant ="John";
+var cookTime = 60;
+var foodItems = ["chicken", "vegetables", "potatoes", "desert"];
+var startTime = true;
+var say = console.log()
+
+// Procedure
+var start = function () {
+	if (startTime = true) {
+		console.log( "The clock has started " +  "You will have " + cookTime + " minutes to Make your dish, Go!!")}
+		
+		else{
+			
+			
+			
+		};
+			
+		
+};
+	
+start();
+
+
+
+for (var i=0; cookTime > i; cookTime--) {
+	
+	console.log("There are "  + cookTime + " minutes Left!");
+};
+
+// Boolean Function
+
+// Number Function
+
+// String Function
+
+// Arrary Function
+
+say.cookTime;
