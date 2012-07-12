ce02e41d42d43166ccd38bf9e7b4bd021001bb38
Josh <jhead12@fullsail.edu>
2012-07-12 16:30:53 -0700

    assignment 2

--- sdi-project2/Head_Joshua_assigment2.js
+++ sdi-project2/Head_Joshua_assigment2.js
@@ -10,7 +10,7 @@
 	
 	# The contestant name var contestant ="John"; had been cooking his first meal in the contest for
 cooking time var cookTime = 20; and he still had to put in the second food item. He only cooked some
-foodItems = [ "chicken" , "vegetables", "potatoes", "desert"].
+foodItems = [ "chicken" , "fruits", "potatoes", "desert"].
 
  Is time to var cookDish = function(meal){...};? It looks like I have var mealsLeft = meals.length; to
 go. The Judges say that there are 30 Mins left, and if the competition (mealsLeft > 0) ends.
@@ -24,7 +24,7 @@ for!"
 
 var contestant ="John";
 var cookTime = 60;
-var foodItems = ["chicken", "vegetables", "potatoes", "desert"];
+var foodItems = ["chicken", "fruits", "potatoes", "desert"];
 var startTime = true;
 var say = function(message){
 	console.log(message);
@@ -33,7 +33,8 @@ var say = function(message){
 // Procedure
 var start = function () {
 	if (startTime) {
-		console.log( "The clock has started " +  "You will have " + cookTime + " minutes to Make your dish, Go!!")}
+		say('The first contestant name is ' + contestant + ' He will making  these 4 Items ' + foodItems );	
+		say( "The clock has started " +  "You will have " + cookTime + " minutes to Make your dish, Go!!")}
 		
 		else{
 			
@@ -46,54 +47,117 @@ var start = function () {
 	
 start();
 
+
+// String Function
+
+var desertNow = function (desert, fruit){
+	
+	var desert1 = desert + " chocolate "
+	var appleTurn = fruit + " pie"
+	
+	say("desert is " + desert1 + appleTurn);
+	
+	 return appleTurn;
+	
+	
+	
+};
+
+
+	say(desertNow("iceCream", "apples"));
+
+
+
 // Number Function
 
 var getTime = function(minutes){
 	
 	
-	while ( minutes > 10){
+	while ( minutes > 20){
 		say(" There are " + minutes + " Left!");
 		minutes-=2;
 		
 		
 };
 
-	if (minutes <= 10) {
-		say("There are lest than 10 minutes to Go!");
-		return minutes;
+	if (minutes == 19) {
+		say("Time is Running Low there " + minutes + " left!");
+		};	
+	while ( (minutes > 0)  &&  (minutes < 19) ) {
 		
-	};
-	
-	say("The contest is Over");
-	
+		say("Time is Running Low there are " + minutes);
+		minutes--;}
+		
+		say("The contest is Over");
+		return minutes;	
 		
 };
+			
+			
+		
+	
+		
 
-getTime(cookTime);
+var minutesLeft = getTime(cookTime);
+
+
+
+
+
+// Arrary Function- all the ingrediance that were used for the contest
+
+var allMeals = function (extra){
+		var ingredient = [ "salt", "pepper", "cummin", "rosemary"];
+		ingredient.push(extra);
+		
+	for (var i=0; i < ingredient.length; i++) {
+			say( "One of the ingredients that were used for these meals is " + ingredient[i])
+	};
+		
+	return ingredient;	
+
+
+};
+
+var ingredient = allMeals("parsly");
+
+say( "Here is the list of Ingredients " + ingredient );
+
+var cookingTime = getTime(cookTime);
+
+
 // Boolean Function
 
 var whatsCooking = function( item, time){
-		var chickenTime = 20;
-		var potatoTime= 15;
-		var timeLeft = Math.floor(time - cookTime);	
-	if ( item != "vegatables" || "desert") {
+		// Estimated CookTimes
 		
-		say( "This" + item +  " should be cooked first! because there is not enough time left!");
-		return;
+		var chickenDone = true;
+		var desertDone = true;
+		var potatoDone = false;
+			
+	if ( time < 20) {
+		
+		
+		say( "The " + item +  " should be cooked first! because there is not enough time because its over smh!" );
+		return time;
 	}
-	else{
-		timeLeft = Math.floor(time - cookTime);
+	if ( (chickenDone && desertDone )|| (chickenDone) ){
+		say("we are Making good Time!");
 	
 	}		
 	return timeLeft;
 
 };
-var whatsDone = whatsCooking(foodItems[3], 10);
+var whatsDone = whatsCooking(foodItems[3], minutesLeft );
 
-say('We are making good time with our meals' + whatsDone)
+say( whatsDone);
 
 
 
-// String Function
 
-// Arrary Function
+
+
+	
+	
+
+
